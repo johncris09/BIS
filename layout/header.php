676c7092b3289c2a23babd77ff731ab8fad31ac8
johncris09 <jcmanabo@gmail>
2019-01-21 11:39:45 +0800

    header

---
+++ layout/header.php
@@ -0,0 +1,58 @@
+<!-- start: header -->
+<header class="header">
+  <div class="logo-container">
+    <a href="../" class="logo">
+    
+      <!-- System Logo -->
+      <!-- <img src="../assets/images/logo.png" height="35" alt="Logo" /> -->
+      
+    </a>
+    <div class="visible-xs toggle-sidebar-left" data-toggle-class="sidebar-left-opened" data-target="html" data-fire-event="sidebar-left-opened">
+      <i class="fa fa-bars" aria-label="Toggle sidebar"></i>
+    </div>
+
+  
+    <div class="visible-xs toggle-sidebar-left" data-toggle-class="sidebar-left-opened" data-target="html" data-fire-event="sidebar-left-opened">
+      <i class="fa fa-bars" aria-label="Toggle sidebar"></i>
+    </div>
+  </div>
+
+  <!-- start: search & user box -->
+  <div class="header-right">
+    
+  
+    <span class="separator"></span>
+
+    <div id="userbox" class="userbox">
+      <a href="#" data-toggle="dropdown">
+        <figure class="profile-picture">
+          <img src="../assets/images/sample-user.jpg" alt="John Cris" class="img-circle" data-lock-picture="../assets/images/sample-user.jpg" /> 
+        </figure>
+        <div class="profile-info" data-lock-name="<?php echo 'John Cris' ;?>" data-lock-email="jcmanabo@gmail.com">
+          <span class="name">John Cris Manabo </span>
+            
+          <span class="role">Role Type</span>
+        </div>
+
+        <i class="fa custom-caret"></i>
+      </a>
+
+      <div class="dropdown-menu">
+        <ul class="list-unstyled">
+          <li class="divider"></li>
+          <li>
+            <a role="menuitem" tabindex="-1" href="#"><i class="fa fa-user"></i> My Profile</a>
+          </li>
+          <li>
+            <a role="menuitem" tabindex="-1" href="#" data-lock-screen="true"><i class="fa fa-lock"></i> Lock Screen</a>
+          </li>
+          <li>
+            <a role="menuitem" tabindex="-1" href="../../index.php"><i class="fa fa-power-off"></i> Logout</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <!-- end: search & user box -->
+</header>
+<!-- end: header -->
