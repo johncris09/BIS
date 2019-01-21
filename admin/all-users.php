15ecbba069dbf9caf6938e22a7e9d275fd6fcb32
johncris09 <jcmanabo@gmail.com>
2019-01-21 20:37:53 +0800

    placeholder user

---
+++ admin/all-users.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Dasboard</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<!-- Specific Page Vendor CSS -->
+	<link rel="stylesheet" href="../assets/vendor/select2/select2.css" />
+	<link rel="stylesheet" href="../assets/vendor/jquery-datatables-bs3/assets/css/datatables.css" />
+
+	<?php include('../component/csslink.php'); ?>
+
+</head>
+	<body>
+		<section class="body">
+
+			<!-- start: header -->
+			<?php include('../layout/header.php'); ?>
+			<!-- end: header -->
+
+			<div class="inner-wrapper">
+				<!-- start: sidebar -->
+				<?php include('../layout/sidebar-left.php'); ?>
+				<!-- end: sidebar -->
+
+				<section role="main" class="content-body">
+					<header class="page-header">
+						<h2>All Users</h2>
+					
+						<div class="right-wrapper pull-right">
+							<ol class="breadcrumbs">
+								<li>
+									<a href="index.php">
+										<i class="fa fa-home"></i>
+									</a>
+								</li>
+								<li><span>Users</span></li>
+								<li><span>All Users</span></li>
+							</ol>
+					
+							<a class="sidebar-right-toggle" data-open="sidebar-right"><i class="fa fa-chevron-left"></i></a>
+						</div>
+					</header>
+
+					<!-- start: page -->
+						<section class="panel">
+							<header class="panel-heading">
+								<div class="panel-actions">
+									<a href="#" class="fa fa-caret-down"></a>
+									<a href="#" class="fa fa-times"></a>
+								</div>
+						
+								<h2 class="panel-title">List of All User</h2>
+							</header>
+							<div class="panel-body">
+								<table class="table table-bordered table-striped mb-none" id="datatable-default">
+									<thead>
+										<tr>
+											<th>Username</th>
+											<th>Password</th>
+											<th>Email</th>
+											<th>Registered</th>
+											<th>Status</th>
+										</tr>
+									</thead>
+									<tbody>
+										<?php for($i=1;$i<=20;$i++){ ?>
+										<tr class="gradeA">
+											<td>john</td>
+											<td>$897328hjehfjehwr889</td>
+											<td>email@email.com</td>
+											<td class="center hidden-phone">2019-01-92 12-12-12</td>
+											<td class="center hidden-phone">0</td>
+										</tr>
+										<?php } ?>
+									</tbody>
+								</table>
+							</div>
+						</section>
+						
+					<!-- end: page -->
+				</section>
+			</div>
+
+			<?php include('../layout/sidebar-right.php'); ?>
+		</section>
+
+		<!-- Vendor -->
+		<?php include('../component/jslink.php'); ?>
+		
+		<!-- Specific Page Vendor -->
+		<script src="../assets/vendor/select2/select2.js"></script>
+		<script src="../assets/vendor/jquery-datatables/media/js/jquery.dataTables.js"></script>
+		<script src="../assets/vendor/jquery-datatables/extras/TableTools/js/dataTables.tableTools.min.js"></script>
+		<script src="../assets/vendor/jquery-datatables-bs3/assets/js/datatables.js"></script>
+		
+		<!-- Theme Base, Components and Settings -->
+		<script src=" ../assets/javascripts/theme.js"></script>
+		
+		<!-- Theme Custom -->
+		<script src=" ../assets/javascripts/theme.custom.js"></script>
+		
+		<!-- Theme Initialization Files -->
+		<script src=" ../assets/javascripts/theme.init.js"></script>
+
+
+		<!-- Examples -->
+		<script src="../assets/javascripts/tables/examples.datatables.default.js"></script>
+		<script src="../assets/javascripts/tables/examples.datatables.row.with.details.js"></script>
+		<script src="../assets/javascripts/tables/examples.datatables.tabletools.js"></script>
+	</body>
+</html>
