2f40f1b2c574c9891facbfc18b779c206a032694
johncris09 <jcmanabo@gmail.com>
2019-01-21 11:48:44 +0800

    script:src for login page

---
+++ assets/jslink.php
@@ -0,0 +1,25 @@
+    
+    
+    <!-- Vendor -->
+    <script src=" assets/vendor/jquery/jquery.js"></script>
+		<script src=" assets/vendor/jquery-browser-mobile/jquery.browser.mobile.js"></script>
+		<script src=" assets/vendor/bootstrap/js/bootstrap.js"></script>
+		<script src=" assets/vendor/nanoscroller/nanoscroller.js"></script>
+		<!-- <script src=" assets/vendor/bootstrap-datepicker/js/bootstrap-datepicker.js"></script> -->
+		<script src=" assets/vendor/magnific-popup/magnific-popup.js"></script>
+		<script src=" assets/vendor/jquery-placeholder/jquery.placeholder.js"></script>
+		
+		
+		
+		<!-- Theme Base, Components and Settings -->
+		<script src=" assets/javascripts/theme.js"></script>
+		
+		<!-- Theme Custom -->
+		<script src=" assets/javascripts/theme.custom.js"></script>
+		
+		<!-- Theme Initialization Files -->
+		<script src=" assets/javascripts/theme.init.js"></script>
+
+
+		<!-- Examples -->
+		<script src=" assets/javascripts/dashboard/examples.dashboard.js"></script>
