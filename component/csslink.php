bde5abe241df563fbd9b48995fd351843a7659ee
johncris09 <jcmanabo@gmail.com>
2019-01-21 21:10:17 +0800

    initial link for css

--- component/csslink.php
+++ component/csslink.php
@@ -1,26 +1,31 @@
 
+		<!-- Mobile Metas -->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+
 		<!-- Web Fonts  -->
 		<link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800|Shadows+Into+Light" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Coiny|Lato|Montserrat|Open+Sans|Raleway|Roboto|Sarabun" rel="stylesheet">
-  
+
 		<!-- Vendor CSS -->
-		
 		<link rel="stylesheet" href="../assets/vendor/bootstrap/css/bootstrap.css" />
 		<link rel="stylesheet" href="../assets/vendor/font-awesome/css/font-awesome.css" />
 		<link rel="stylesheet" href="../assets/vendor/magnific-popup/magnific-popup.css" />
 		<link rel="stylesheet" href="../assets/vendor/bootstrap-datepicker/css/datepicker3.css" />
 
-		
+
+
 		<!-- Theme CSS -->
 		<link rel="stylesheet" href="../assets/stylesheets/theme.css" />
 
 		<!-- Skin CSS -->
-		<link rel="stylesheet" href="../assets/stylesheets/skins/extension.css" />
+		<link rel="stylesheet" href="../assets/stylesheets/skins/default.css" />
 
 		<!-- Theme Custom CSS -->
 		<link rel="stylesheet" href="../assets/stylesheets/theme-custom.css">
 
 		<!-- Head Libs -->
-    <script src="../assets/vendor/modernizr/modernizr.js"></script>
+		<script src="../assets/vendor/modernizr/modernizr.js"></script>
+
+		
+		
     
     
