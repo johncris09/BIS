3dfdc6359e3b2f92d4c4fbd8c7232940b676fb6c
johncris09 <jcmanabo@gmail.com>
2019-01-22 08:41:32 +0800

    barangay issue

---
+++ layout/sidebar-left.php
@@ -0,0 +1,162 @@
+<aside id="sidebar-left" class="sidebar-left">
+				
+  <div class="sidebar-header">
+    <div class="sidebar-title">
+      Navigation
+    </div>
+    <div class="sidebar-toggle hidden-xs" data-toggle-class="sidebar-left-collapsed" data-target="html" data-fire-event="sidebar-left-toggle">
+      <i class="fa fa-tasks" aria-label="Toggle sidebar"></i>
+    </div>
+  </div>
+
+  <div class="nano">
+    <div class="nano-content">
+      <nav id="menu" class="nav-main" role="navigation">
+        <ul class="nav nav-main">
+          <li class="nav-active">
+            <a href="index.php">
+              <i class="fa fa-dashboard" aria-hidden="true"></i>
+              <span>Dashboard</span>
+            </a>
+          </li>
+          <li class="nav-parent">
+            <a>
+              <i class="fa fa-map-marker" aria-hidden="true"></i>
+              <span>Barangay &amp; Streets</span>
+            </a>
+            <ul class="nav nav-children">
+              <li>
+                <a href="barangay.php">
+                    Barangay
+                </a>
+              </li>
+              <li>
+                <a href="street.php">
+                  Street
+                </a>
+              </li>
+            </ul>
+          </li>
+          
+          <li class="nav-parent">
+            <a>
+              <i class="fa fa-user" aria-hidden="true"></i>
+              <span>User</span>
+            </a>
+            <ul class="nav nav-children">
+              <li>
+                <a href="all-users.php">
+                    All Users
+                </a>
+              </li>
+              <li>
+                <a href="new-user.php">
+                    Add New User
+                </a>
+              </li>
+              <li>
+                <a href="profile.php">
+                    Your Profile
+                </a>
+              </li>
+            </ul>
+          </li>
+
+          
+          <li class="nav-parent">
+            <a>
+              <i class="fa fa-gears" aria-hidden="true"></i>
+              <span>Utilities</span>
+            </a>
+            <ul class="nav nav-children">
+              <li>
+                <a href="back-up-records.php">
+                    Back up Records
+                </a>
+              </li>
+            </ul>
+          </li>
+
+          
+          
+        </ul>
+      </nav>
+      
+
+      <ul class="nav nav-main">
+        <li class="nav-parent">
+          <a>
+            <i class="fa fa-users" aria-hidden="true"></i>
+            <span>Residence Profiling</span>
+          </a>
+          <ul class="nav nav-children">
+            <li>
+              <a href="">
+                  All Resident
+              </a>
+            </li>
+            <li>
+              <a href="">
+                  Add New Resident
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li class="nav-parent">
+          <a>
+            <i class="fa fa-users" aria-hidden="true"></i>
+            <span>Barangay Issue</span>
+          </a>
+          <ul class="nav nav-children">
+            <li>
+              <a href="">
+                  All Issue
+              </a>
+            </li>
+            <li>
+              <a href="">
+                  Add New Issude
+              </a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+
+      <hr class="separator" />
+      <ul class="nav nav-main">
+        <li class="nav-parent">
+          <a>
+            <i class="fa fa-columns" aria-hidden="true"></i>
+            <span>Layouts</span>
+          </a>
+          <ul class="nav nav-children">
+            <li>
+              <a href="layouts-default.html">
+                  Default
+              </a>
+            </li>
+            <li>
+              <a href="layouts-boxed.html">
+                  Boxed
+              </a>
+            </li>
+            <li>
+              <a href="layouts-menu-collapsed.html">
+                  Menu Collapsed
+              </a>
+            </li>
+            <li>
+              <a href="layouts-scroll.html">
+                  Scroll
+              </a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+      
+      
+    </div>
+
+  </div>
+
+</aside>
