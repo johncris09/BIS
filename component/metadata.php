5f8d2857a5aa7b2b5f21e632626a957d9c1a8019
johncris09 <jcmanabo@gmail.com>
2019-01-24 19:37:38 +0800

    icon

--- component/metadata.php
+++ component/metadata.php
@@ -1,3 +1,6 @@
-  <meta charset="utf-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+      
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="../assets/images/logo.png" type="image/x-icon" />
+      
