0a990f8d7aab6d03fde34c21be153ab4709b0f59
johncris09 <jcmanabo@gmail.com>
2019-01-24 19:37:12 +0800

    theme:js link

---
+++ component/themejslink.php
@@ -0,0 +1,9 @@
+    
+    <!-- Theme Base, Components and Settings -->
+		<script src=" ../assets/javascripts/theme.js"></script>
+		
+		<!-- Theme Custom -->
+		<script src=" ../assets/javascripts/theme.custom.js"></script>
+		
+		<!-- Theme Initialization Files -->
+		<script src=" ../assets/javascripts/theme.init.js"></script>
