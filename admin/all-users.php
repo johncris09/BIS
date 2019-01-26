3d292d95d80730175ad24cbad633c38ab4a1f5b3
johncris09 <jcmanabo@gmail.com>
2019-01-26 12:57:04 +0800

    fixed:problem in responsive

--- admin/all-users.php
+++ admin/all-users.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<title>All Users</title>
+<html class="fixed">
+	<head>
+		<title>All Users</title>
 
-	<?php include('../component/metadata.php'); ?>
-	
-	
-	<!-- Specific Page Vendor CSS -->
-	<link rel="stylesheet" href="../assets/vendor/select2/select2.css" />
-	<link rel="stylesheet" href="../assets/vendor/jquery-datatables-bs3/assets/css/datatables.css" />
+		<?php include('../component/metadata.php'); ?>
 
-	<?php include('../component/csslink.php'); ?>
+		<?php include('../component/csslink.php'); ?>
+		<!-- Specific Page Vendor CSS -->
+		<link rel="stylesheet" href="../assets/vendor/select2/select2.css" />
+		<link rel="stylesheet" href="../assets/vendor/jquery-datatables-bs3/assets/css/datatables.css" />
 
-</head>
+		<?php include('../component/cssthemelink.php'); ?>
+
+	</head>
 	<body>
 		<section class="body">
 
@@ -27,17 +27,17 @@
 
 				<section role="main" class="content-body">
 					<header class="page-header">
-						<h2>All Users</h2>
+						<h2>All User</h2>
 					
 						<div class="right-wrapper pull-right">
 							<ol class="breadcrumbs">
 								<li>
-									<a href="index.php">
+									<a href="index.html">
 										<i class="fa fa-home"></i>
 									</a>
 								</li>
-								<li><span>Users</span></li>
-								<li><span>All Users</span></li>
+								<li><span>User</span></li>
+								<li><span>All User</span></li>
 							</ol>
 					
 							<a class="sidebar-right-toggle" data-open="sidebar-right"><i class="fa fa-chevron-left"></i></a>
@@ -60,36 +60,38 @@
 										<a href="new-user.php" title="Add New User" class="btn btn-primary mb-sm mt-xs mr-xs">Add New User</a>
 									</div>
 								</div>
-								
 								<table class="table table-bordered table-hover table-striped mb-none" id="datatable-default">
-									<thead>
-										<tr>
-											<th>Name</th>
-											<th>Username</th>
-											<th>Email</th>
-											<th>Registered</th>
-											<th>Status</th>
-											<th>Action</th>
-										</tr>
-									</thead>
-									<tbody>
-										<?php for($i=1;$i<=20;$i++){ ?>
-										<tr class="gradeA">
-										<td>Juan Tamad</td>
-											<td>john</td>
-											<td>email@email.com</td>
-											<td>2019-01-22 05:36:18</td>
-											<td>Administrator</td>
-											<td>
-												<ul class="list-inline">
-													<li><a class="text-warning" href="edit-user.php"> <i class="fa fa-pencil" aria-hidden="true"></i> Edit </a></li>
-													<li><a class="text-danger" href="delete-user.php"> <i class="fa fa-eye" aria-hidden="true"></i> Delete</a></li>
-												</ul>
-											</td>
-										</tr>
-										<?php } ?>
-									</tbody>
-								</table>
+								<thead>
+									<tr>
+                    <th>#</th>
+										<th>Name</th>
+										<th>Username</th>
+										<th>Email</th>
+										<th>Registered</th>
+										<th>Status</th>
+										<th>Action</th>
+									</tr>
+								</thead>
+								<tbody>
+									<?php for($i=1;$i<=20;$i++){ ?>
+									<tr class="gradeA">
+                    <th><? echo $i; ?></th>
+                    <td>Juan Tamad</td>
+										<td>john</td>
+										<td>email@email.com</td>
+										<td>2019-01-22 05:36:18</td>
+										<td>Administrator</td>
+										<td>
+											<ul class="list-inline">
+												<li><a class="btn text-warning" href="edit-user.php"> <i class="fa fa-pencil" aria-hidden="true"></i> Edit </a></li>
+												<!-- on:click('delete'){Call Modal} -->
+												<li><a class="btn text-danger" href="#"> <i class="fa fa-trash-o" aria-hidden="true"></i> Delete</a></li>
+											</ul>
+										</td>
+									</tr>
+									<?php } ?>
+								</tbody>
+							</table>
 							</div>
 						</section>
 						
@@ -100,21 +102,15 @@
 			<?php include('../layout/sidebar-right.php'); ?>
 		</section>
 
-		<!-- Vendor -->
-		<?php include('../component/jslink.php'); ?>
-		
+		<?php  include('../component/jslink.php'); ?>
 		<!-- Specific Page Vendor -->
 		<script src="../assets/vendor/select2/select2.js"></script>
 		<script src="../assets/vendor/jquery-datatables/media/js/jquery.dataTables.js"></script>
-		<script src="../assets/vendor/jquery-datatables/extras/TableTools/js/dataTables.tableTools.min.js"></script>
 		<script src="../assets/vendor/jquery-datatables-bs3/assets/js/datatables.js"></script>
 		
-		<?php include('../component/themejslink.php');  ?>
-
+		<?php include('../component/themejslink.php'); ?>
 
 		<!-- Examples -->
 		<script src="../assets/javascripts/tables/examples.datatables.default.js"></script>
-		<script src="../assets/javascripts/tables/examples.datatables.row.with.details.js"></script>
-		<script src="../assets/javascripts/tables/examples.datatables.tabletools.js"></script>
-	</body>
+		</body>
 </html>
