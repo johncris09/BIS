44cf6b743cec067fe0380e1b5e37a5fbf41affbf
johncris09 <jcmanabo@gmail.com>
2019-01-26 12:59:05 +0800

    link:csstheme

---
+++ component/cssthemelink.php
@@ -0,0 +1,11 @@
+	<!-- Theme CSS -->
+  <link rel="stylesheet" href="../assets/stylesheets/theme.css" />
+
+  <!-- Skin CSS -->
+  <link rel="stylesheet" href="../assets/stylesheets/skins/default.css" />
+
+  <!-- Theme Custom CSS -->
+  <link rel="stylesheet" href="../assets/stylesheets/theme-custom.css">
+
+  <!-- Head Libs -->
+  <script src="../assets/vendor/modernizr/modernizr.js"></script>
