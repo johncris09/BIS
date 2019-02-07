a5b9d8137662f26dce5a3fb022a28c28607d70f3
johncris09 <jcmanabo@gmail.com>
2019-02-07 21:44:04 +0800

    List of all using Barangay Using id

--- admin/barangay.php
+++ admin/barangay.php
@@ -8,8 +8,8 @@
 		<?php include('../component/csslink.php'); ?>
 		<link rel="stylesheet" href="../assets/vendor/select2/select2.css" />
 		<link rel="stylesheet" href="../assets/vendor/jquery-datatables-bs3/assets/css/datatables.css" />
-				
-		<?php include('../component/cssthemelink.php'); ?>
+		<link rel="stylesheet" href="../assets/vendor/pnotify/pnotify.custom.css" />
+		<?php include('../component/csslink.php'); ?>
 
 	</head>
 	<body>
@@ -48,30 +48,31 @@
 					
 					<div class="row">
 							<div class="col-md-5">
-								<form id="form1" class="form-horizontal">
+								<form id="form_barangay" class="form-horizontal">
 									<section class="panel panel-featured panel-featured-primary">
 										<header class="panel-heading">
 											<div class="panel-actions">
 												<a href="#" class="fa fa-caret-down"></a>
 												<a href="#" class="fa fa-times"></a>
 											</div>
-											<h2 class="panel-title">Add Barangay</h2>
+											<h2 class="panel-title" id="panel-title-barangay">Add Barangay</h2>
 										</header>
 										<div class="panel-body" style="display: block;">
 											<div class="form-group">
-												<label class="col-sm-4 control-label">Barangay Name </label>
+												<label class="col-sm-4 control-label" id="label">Barangay Name </label>
 												<div class="col-sm-8">
-													<input type="text" name="add_barangay" placeholder="Barangay Name" class="form-control" required autofocus>
+													<input type="hidden" id="barangay_id"  class="form-control">
+													<input type="text" id="barangay" name="barangay" placeholder="Barangay Name"  class="form-control" required autofocus >
+													<label class="error" id="err_msgs"></label>
 												</div>
 											</div>
 										</div>
 										<footer class="panel-footer" style="display: block;">
 											<div class="row">
 												<div class="col-sm-12 text-right">
-													<button type="submit" class="btn btn-primary hidden-xs mb-xs mt-xs mr-xs "><i class="fa fa-save"></i> Save</button>
-													<button type="submit" class="btn btn-default hidden-xs mb-xs mt-xs mr-xs "> Reset</button>
-													<button type="submit" class="btn btn-primary btn-block  visible-xs mb-xs mt-xs mr-xs"><i class="fa fa-save"></i>  Save</button>
-													<button type="submit" class="btn btn-default btn-block  visible-xs mb-xs mt-xs mr-xs"> Reset</button>
+													<button type="button" name="update_barangay" id="update_barangay" class="btn btn-primary  mb-xs mt-xs mr-xs "><i class="fa fa-edit"></i> Update</button>
+													<button type="button" name="add_new_barangay" id="add_new_barangay" class="btn btn-primary   mb-xs mt-xs mr-xs "><i class="fa fa-save"></i> Save</button>
+													<button type="button" id="reset_barangay" class="btn btn-default  mb-xs mt-xs mr-xs "> Reset</button>
 												</div>
 											</div>
 										</footer>
@@ -90,30 +91,29 @@
 									<h2 class="panel-title">List of All Barangay</h2>
 								</header>
 								<div class="panel-body">
-									<table class="table table-bordered table-striped mb-none"  id="datatable-default">
-										<thead>
-											<tr>
-												<th>#</th>
-												<th>Barangay Name</th>
-												<th>Action</th>
-											</tr>
-										</thead>
-										<tbody>
-											<?php for($i=1;$i<=5;$i++){ ?>
-											<tr class="gradeX">
-												<td><? echo $i; ?></td>
-												<td>Barangay <? echo $i; ?></td>
-												<td>
-													<ul class="list-inline">
-														<li><a class="text-warning" href=""> <i class="fa fa-pencil" aria-hidden="true"></i> Edit </a></li>
-														<!-- on:click('delete'){Call Modal} -->
-														<li><a class="text-danger" href="#"> <i class="fa fa-trash-o" aria-hidden="true"></i> Delete</a></li>
-													</ul>
-												</td>
-												</tr>
-											<?php } ?>
-										</tbody>
-									</table>
+                  <div id="list_of_barangay"></div>
+								</div>
+								<!-- Modal -->
+								<div class="modal fade" id="deleteBarangayModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+									<div class="modal-dialog">
+										<div class="modal-content">
+											<div class="modal-header">
+												<div class="modal-wrapper">
+													<div class="modal-icon center ">
+														<i class="text-primary fa fa-question-circle"></i>
+													</div>
+													<div class="modal-text text-center">
+														<h4>Are you sure?</h4>
+														<p>Are you sure that you want to delete this Barangay <span id="delete_barangay_id"></span>?</p>
+													</div>
+												</div>
+											</div>
+											<div class="modal-footer">
+												<button type="button" id="confirm-delete-Barangay" class="btn btn-primary">Confirm</button>
+												<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+											</div>
+										</div>
+									</div>
 								</div>
 							</section>
 						</div>
@@ -128,21 +128,19 @@
 			<?php include('../layout/sidebar-right.php'); ?>
 		</section>
 
-		<!-- Vendor -->
-		<?php include('../component/jslink.php'); ?>
+	<!-- Vendor -->
+		
+	<?php include('../component/jslink.php'); ?>
 		
 		<!-- Specific Page Vendor -->
+		<script src="../assets/vendor/pnotify/pnotify.custom.js"></script>
 		<script src="../assets/vendor/select2/select2.js"></script>
 		<script src="../assets/vendor/jquery-datatables/media/js/jquery.dataTables.js"></script>
-		<script src="../assets/vendor/jquery-datatables/extras/TableTools/js/dataTables.tableTools.min.js"></script>
 		<script src="../assets/vendor/jquery-datatables-bs3/assets/js/datatables.js"></script>
 		
+		
+
 		<?php include('../component/themejslink.php');  ?>
-
-
-		<!-- Examples -->
-		<script src="../assets/javascripts/tables/examples.datatables.default.js"></script>
-		<script src="../assets/javascripts/tables/examples.datatables.row.with.details.js"></script>
-		<script src="../assets/javascripts/tables/examples.datatables.tabletools.js"></script>
+		<script src="../assets/vendor/jquery-ui/js/jquery-ui.1.12.1.js"></script> 
 	</body>
 </html>
