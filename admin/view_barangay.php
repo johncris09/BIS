a6a2c6e1181161e8fc78fec9977ab3387ba4d32a
johncris09 <jcmanabo@gmail.com>
2019-02-09 14:05:31 +0800

    View Barangay

---
+++ admin/view_barangay.php
@@ -0,0 +1,50 @@
+<?php
+  
+  include_once '../classes/Database.php';
+  include_once '../classes/Barangay.php';
+  include_once '../classes/initial.php';
+  
+  
+  $barangay = new Barangay($db);
+  $prep_state = $barangay->getAllBarangay();
+
+  $counter=1;
+  $list = '
+    <table class="table table-bordered table-striped mb-none" id="datatable-default" >
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Positon</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+  ';
+  while ($row = $prep_state->fetch(PDO::FETCH_ASSOC))
+  {
+    $list .= '
+        <tr>
+          <td>'.$counter.'</td>
+          <td>'.$row['barangay_name'].'</td>
+          <td>
+            <ul class="list-inline">
+              <li><a id="' . $row['barangay_id'] . '" class="text-warning edit-barangay"> <i class="fa fa-pencil" aria-hidden="true"></i> Edit </a></li>
+              <li><a id="' . $row['barangay_id'] . '"  class="text-danger delete-barangay"> <i class="fa fa-trash-o" aria-hidden="true"></i> Delete</a></li>
+            </ul>
+          </td>
+        </tr>
+    ';
+    $counter++;
+  }
+  $list = $list . '
+      </tbody>
+    </table>
+  ';
+
+  echo $list;
+
+?>
+  
+
+		<script src="../assets/javascripts/tables/examples.datatables.default.js"></script>
+		
