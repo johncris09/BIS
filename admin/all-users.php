e361ad02187cff47580bb3aaba7863b029507a08
johncris09 <jcmanabo@gmail.com>
2019-02-10 17:55:36 +0800

    Can view all the users with their information

--- admin/all-users.php
+++ admin/all-users.php
@@ -3,14 +3,14 @@
 	<head>
 		<title>All Users</title>
 
-		<?php include('../component/metadata.php'); ?>
-
-		<?php include('../component/csslink.php'); ?>
 		<!-- Specific Page Vendor CSS -->
+		<?php include('../component/csslink.php'); ?>
 		<link rel="stylesheet" href="../assets/vendor/select2/select2.css" />
 		<link rel="stylesheet" href="../assets/vendor/jquery-datatables-bs3/assets/css/datatables.css" />
+		<link rel="stylesheet" href="../assets/vendor/pnotify/pnotify.custom.css" />
+		<?php include('../component/csslink.php'); ?>
 
-		<?php include('../component/cssthemelink.php'); ?>
+		
 
 	</head>
 	<body>
@@ -60,38 +60,9 @@
 										<a href="new-user.php" title="Add New User" class="btn btn-primary mb-sm mt-xs mr-xs">Add New User</a>
 									</div>
 								</div>
-								<table class="table table-bordered table-hover table-striped mb-none" id="datatable-default">
-								<thead>
-									<tr>
-                    <th>#</th>
-										<th>Name</th>
-										<th>Username</th>
-										<th>Email</th>
-										<th>Registered</th>
-										<th>Status</th>
-										<th>Action</th>
-									</tr>
-								</thead>
-								<tbody>
-									<?php for($i=1;$i<=20;$i++){ ?>
-									<tr class="gradeA">
-                    <th><? echo $i; ?></th>
-                    <td>Juan Tamad</td>
-										<td>john</td>
-										<td>email@email.com</td>
-										<td>2019-01-22 05:36:18</td>
-										<td>Administrator</td>
-										<td>
-											<ul class="list-inline">
-												<li><a class="text-warning" href="edit-user.php"> <i class="fa fa-pencil" aria-hidden="true"></i> Edit </a></li>
-												<!-- on:click('delete'){Call Modal} -->
-												<li><a class="text-danger" href="#"> <i class="fa fa-trash-o" aria-hidden="true"></i> Delete</a></li>
-											</ul>
-										</td>
-									</tr>
-									<?php } ?>
-								</tbody>
-							</table>
+								<div class="panel-body">
+                  <div id="list_of_user"></div>
+								</div>
 							</div>
 						</section>
 						
@@ -102,15 +73,19 @@
 			<?php include('../layout/sidebar-right.php'); ?>
 		</section>
 
-		<?php  include('../component/jslink.php'); ?>
+		<!-- Vendor -->
+			
+		<?php include('../component/jslink.php'); ?>
+		
 		<!-- Specific Page Vendor -->
+		<script src="../assets/vendor/pnotify/pnotify.custom.js"></script>
 		<script src="../assets/vendor/select2/select2.js"></script>
 		<script src="../assets/vendor/jquery-datatables/media/js/jquery.dataTables.js"></script>
 		<script src="../assets/vendor/jquery-datatables-bs3/assets/js/datatables.js"></script>
 		
-		<?php include('../component/themejslink.php'); ?>
+		
 
-		<!-- Examples -->
-		<script src="../assets/javascripts/tables/examples.datatables.default.js"></script>
+		<?php include('../component/themejslink.php');  ?>
+		<script src="../assets/vendor/jquery-ui/js/jquery-ui.1.12.1.js"></script> 
 		</body>
 </html>
