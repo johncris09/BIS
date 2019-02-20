71dda7597b0e33f98c1524279fec16b1ee785f58
johncris09 <jcmanabo@gmail.com>
2019-02-20 23:13:11 +0800

    All Resident with in the barangay

--- staff/all-resident.php
+++ staff/all-resident.php
@@ -1,22 +1,22 @@
+<?php  session_start(); ?>
 <!DOCTYPE html>
 <html class="fixed">
 	<head>
 		<title>All Resident</title>
 
-		<?php include('../component/metadata.php'); ?>
-
-		<?php include('../component/csslink.php'); ?>
 		<!-- Specific Page Vendor CSS -->
+		<?php include('../component/csslink.php'); ?>
 		<link rel="stylesheet" href="../assets/vendor/select2/select2.css" />
 		<link rel="stylesheet" href="../assets/vendor/jquery-datatables-bs3/assets/css/datatables.css" />
-
-		<?php include('../component/cssthemelink.php'); ?>
+		<link rel="stylesheet" href="../assets/vendor/pnotify/pnotify.custom.css" />
+		<?php include('../component/csslink.php'); ?>
+  
 
 	</head>
 	<body>
 		<section class="body">
 
-			<!-- start: header -->
+		  	<!-- start: header -->
 			<?php include('../layout/header.php'); ?>
 			<!-- end: header -->
 
@@ -32,20 +32,19 @@
 						<div class="right-wrapper pull-right">
 							<ol class="breadcrumbs">
 								<li>
-									<a href="index.html">
+									<a href="index.php">
 										<i class="fa fa-home"></i>
 									</a>
 								</li>
-								<li><span>Residence Profiling</span></li>
+								<li><span> Residence Profiling</span></li>
 								<li><span>All Resident</span></li>
 							</ol>
 					
 							<a class="sidebar-right-toggle" data-open="sidebar-right"><i class="fa fa-chevron-left"></i></a>
 						</div>
-					</header>
-
-					<!-- start: page -->
-						<section class="panel">
+          </header>
+          
+          <section class="panel panel-featured panel-featured-primary">
 							<header class="panel-heading">
 								<div class="panel-actions">
 									<a href="#" class="fa fa-caret-down"></a>
@@ -60,52 +59,10 @@
 										<a href="new-resident.php" title="Add New Resident" class="btn btn-primary mb-sm mt-xs mr-xs">Add New Resident</a>
 									</div>
 								</div>
-								<table class="table table-bordered table-striped mb-none" id="datatable-default">
-									<thead>
-										<tr>
-											<th>#</th>
-											<th>Profile	</th>
-											<th>Last Name</th>
-											<th>First Name</th>
-											<th>Middle Name</th>
-											<th>Extension</th>
-											<th>House Number</th>
-											<th>Street</th>
-											<th>Birthplace</th>
-											<th>Birthdate</th>
-											<th>Status</th>
-											<th>Civil Status</th>
-											<th>Citizenship</th>
-											<th>Phone Number</th>
-											<th>Occupation</th>
-											<th>Action</th>
-										</tr>
-									</thead>
-									<tbody>
-										<? for($i=1;$i<50;$i++){ ?>
-										<tr class="gradeX">
-											<th><? echo $i;?></th>
-											<td>
-											<img src="../assets/images/sample-user.jpg" width="40" height="40" alt="John Cris'sProfile Picture" /> 
-											</td>
-											<td>Manabo</td>
-											<td>John Cris</td>
-											<td>Calamongay</td>
-											<td>II</td>
-											<td>238423</td>
-											<td>Purok 1</td>
-											<td>MOPH</td>
-											<td>1999-01-01</td>
-											<td>Male</td>
-											<td>Single</td>
-											<td>Filipino</td>	
-											<td>082903123</td>
-											<td>Student</td>
-											<td>View Delete Edit</td>
-										</tr>
-										<? } ?>
-									</tbody>
-								</table>
+								<div class="panel-body">
+									<div id="lisf-of-resident"></div>
+								</div>
+								
 							</div>
 						</section>
 						
@@ -116,15 +73,19 @@
 			<?php include('../layout/sidebar-right.php'); ?>
 		</section>
 
-		<?php  include('../component/jslink.php'); ?>
+		<!-- Vendor -->
+			
+		<?php include('../component/jslink.php'); ?>
+		
 		<!-- Specific Page Vendor -->
+		<script src="../assets/vendor/pnotify/pnotify.custom.js"></script>
 		<script src="../assets/vendor/select2/select2.js"></script>
 		<script src="../assets/vendor/jquery-datatables/media/js/jquery.dataTables.js"></script>
 		<script src="../assets/vendor/jquery-datatables-bs3/assets/js/datatables.js"></script>
 		
-		<?php include('../component/themejslink.php'); ?>
+		
 
-		<!-- Examples -->
-		<script src="../assets/javascripts/tables/examples.datatables.default.js"></script>
+		<?php include('../component/themejslink.php');  ?>
+		<script src="../assets/vendor/jquery-ui/js/jquery-ui.1.12.1.js"></script> 
 		</body>
 </html>
