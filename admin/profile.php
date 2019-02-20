3240cd44e2a1904312cd68f36c32ecef31439a26
johncris09 <jcmanabo@gmail.com>
2019-02-20 22:25:15 +0800

    profile page for admin

--- admin/profile.php
+++ admin/profile.php
@@ -1,10 +1,26 @@
+<? 
+  session_start(); 
+  include_once '../classes/Database.php';
+  include_once  '../classes/Position.php';
+  include_once  '../classes/Barangay.php'; 
+  include_once  '../classes/initial.php'; 
+
+  $position = new Position($db);
+  $barangay = new Barangay($db);
+  
+  // Ternary Operator
+  $edit_account_id  = isset($_SESSION['edit_account_id'])?$_SESSION['edit_account_id']: null ;
+  $edit_user_id     =isset($_SESSION['edit_user_id'])?$_SESSION['edit_user_id']: null ;
+?>
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Profile</title>
-    <?php include('../component/metadata.php'); ?>
-    
-    <?php include('../component/csslink.php'); ?>
+		<title>Profile</title>
+		<?php include('../component/metadata.php'); ?>
+		<!-- Specific Page Vendor CSS -->
+		<?php include('../component/csslink.php'); ?>
+		<link rel="stylesheet" href="../assets/vendor/pnotify/pnotify.custom.css" />
+		<?php include('../component/csslink.php'); ?>
     
   </head>
   <body>
@@ -78,48 +94,54 @@
 										<form class="form-horizontal" method="post">
 											<fieldset>
 												<div class="form-group">
-													<label class="col-md-3 control-label" for="profileFirstName">First Name</label>
+													<label class="col-md-3 control-label" for="first_name">First Name</label>
 													<div class="col-md-8">
-														<input type="text" class="form-control" name="first_name" id="profileFirstName" value="John Cris" placeholder="First Name" required>
+														<input type="text" id="first_name" class="form-control" name="first_name" placeholder="First Name" required>
 													</div>
 												</div>
 												<div class="form-group">
-													<label class="col-md-3 control-label" for="profileMiddleName">Middle Name</label>
+													<label class="col-md-3 control-label" for="middle_name">Middle Name</label>
 													<div class="col-md-8">
-														<input type="text" class="form-control" name="middle_name" id="profileMiddleName" value="Calamongay" placeholder="Middle Name">
+														<input type="text" class="form-control" name="middle_name" id="middle_name"  placeholder="Middle Name">
 													</div>
 												</div>
 												<div class="form-group">
-													<label class="col-md-3 control-label" for="profileLastName">Last Name</label>
+													<label class="col-md-3 control-label" for="last_name">Last Name</label>
 													<div class="col-md-8">
-														<input type="text" class="form-control" name="last_name" id="profileLastName" value="Manabo" placeholder="Last Name" required>
+														<input type="text" class="form-control" name="last_name" id="last_name" placeholder="Last Name" required>
 													</div>
 												</div>
 												<div class="form-group">
 													<label class="col-md-3 control-label" for="profileGender">Gender</label>
-													<div class="col-md-3">
-														<select id="profileGender" class="form-control" name="gender" required>
-															<option value="Male">Male</option>
-															<option value="Female">Female</option>
-														</select>
-													</div>
+													<div class="col-sm-9">
+                            <div class="radio-custom radio-primary">
+                              <input id="male" name="gender" type="radio" value="Male" required="" checked="checked">
+                              <label for="male">Male</label>
+                            </div>
+                            <div class="radio-custom radio-primary">
+                              <input id="female" name="gender" type="radio" value="Female" required="">
+                              <label for="female">Female</label>
+                            </div>
+                            <label class="error" for="gender"></label>
+                          </div>
 												</div>
 												<div class="form-group">
-													<label class="col-md-3 control-label" for="profileStatus">Status</label>
-													<div class="col-md-3">
-														<select id="profileStatus" class="form-control" name="status" required>
-															<option value="Single">Single</option>
-															<option value="Married">Married</option>
-															<option value="Widowed">Widowed</option>
-															<option value="Separated">Separated</option>
-														</select>
-													</div>
+													<label class="col-sm-3 control-label" for="civil_status">Status</label>
+                          <div class="col-sm-4">
+                            <select id="civil_status" name="status" class="form-control" required="">
+                            	<option value="Single">Single</option>
+                              <option value="Married">Married</option>
+                              <option value="Widowed">Widowed</option>
+                              <option value="Separated">Separated</option>
+                            </select>
+                            <label class="error" for="status"></label>
+                          </div>
 												</div>
 												<div class="form-group">
-													<label class="col-md-3 control-label" for="profileCitizenship">Citizenship</label>
-													<div class="col-md-3">
-														<input type="text" class="form-control" name="citizenship" id="profileCitizenship" value="Filipino" placeholder="Citizenship" required>
-													</div>
+													<label class="col-sm-3 control-label" for="citizenship">Citizenship</label>
+                          <div class="col-sm-4">
+                            <input type="text" id="citizenship" class="form-control"   placeholder="Citizenship" name="citizenship"  required="">
+                          </div>
 												</div>
 											</fieldset>
 
@@ -128,22 +150,16 @@
 											<h4 class="mb-xlg">Contact Info</h4>
 											<fieldset>
 												<div class="form-group">
-													<label class="col-md-3 control-label" for="profileEmail">Email</label>
-													<div class="col-md-8">
-														<input type="text" class="form-control" name="email" id="profileEmail" value="jcmanabo@gmail.com" required>
-													</div>
-												</div>
-												<div class="form-group">
-													<label class="col-md-3 control-label" for="profilePhoneNumber">Phone Number</label>
-													<div class="col-md-8">
-														<div class="input-group">
-															<span class="input-group-addon">
-																<i class="fa fa-phone"></i>
-															</span>
-															<input id="profilePhoneNumber" name="phone_number" data-plugin-masked-input="" data-input-mask="(+63) 999-999-9999" placeholder="(+63) 123-123-1234" class="form-control">
-													</div>
-													</div>
-												</div>
+												<label class="col-sm-3 control-label" for="email">Email</label>
+                          <div class="col-sm-8">
+                            <div class="input-group input-group-icon ">
+                              <span class="input-group-addon">
+                                <span class="icon"><i class="fa fa-envelope"></i></span>
+                              </span>
+                              <input type="text" class="form-control" placeholder="you@email.com" name="email" id="email"  required="">
+                            </div>
+                          </div>
+                        </div>
 											</fieldset>
 
 											<!-- Role Managament -->
@@ -151,13 +167,13 @@
 											<h4 class="mb-xlg">Role Management</h4>
 											<fieldset>
 												<div class="form-group">
-													<label class="col-md-3 control-label" for="profileRoleType">Role Type</label>
-													<div class="col-md-5">
-														<select id="profileRoleType" class="form-control" name="status" required>
-															<option value="Administration">Administration</option>
-															<option value="Staff">Staff</option>
-														</select>
-													</div>
+													<label class="col-sm-3 control-label" for="role">Role</label>
+                          <div class="col-sm-8">
+                            <select id="role" class="form-control" name="role" required>
+                              <option value="0">Administration</option>
+                              <option value="1">Staff</option>
+                            </select>
+                          </div>
 												</div>
 											</fieldset>
 
@@ -166,10 +182,15 @@
 											<h4 class="mb-xlg">Account Management</h4>
 											<fieldset>
 												<div class="form-group">
-													<label class="col-md-3 control-label" for="profileUsername">Username</label>
-													<div class="col-md-8">
-														<input type="text" class="form-control" placeholder="Username" name="username" id="profileUsername" value="johncris" required>
-													</div>
+													<label class="col-sm-3 control-label" for="usename" >Username</label>
+                          <div class="col-sm-7">
+                            <div class="input-group input-group-icon">
+                              <span class="input-group-addon">
+                                <span class="icon"><i class="fa fa-user"></i></span>
+                              </span>
+                              <input type="text" id="username" class="form-control" placeholder="Username" name="username"  autofocus required="">
+                            </div>
+                          </div>
 												</div>
 											</fieldset>
 
@@ -177,24 +198,36 @@
 											<hr class="dotted tall">
 											<h4 class="mb-xlg">Change Password</h4>
 											<fieldset class="mb-xl">
-												<div class="form-group">
-													<label class="col-md-3 control-label" for="profileNewPassword">New Password</label>
+												<!-- Old Password -->
+												<div class="form-group" id="old-password-form">
+													<label class="col-md-3 control-label" for="old_password">Old Password</label>
 													<div class="col-md-8">
-														<input type="text" class="form-control" id="profileNewPassword" minlength="6">
+														<div class="input-group mb-md">
+														<div class="input-group input-group-icon">
+                              <span class="input-group-addon">
+                                <span class="icon"><i class="fa fa-key"></i></span>
+                              </span>
+															<input type="password" class="form-control" id="old_password" placeholder="Old Password"  >	
+														</div>
+															<!-- <span class="icon"><i class="fa fa-key"></i></span> -->
+															<span  style="cursor: pointer" class="input-group-addon btn-primary" id="check_password"> <i class="fa fa-question" aria-hidden="true"></i> Check Password</span>
+														</div>
+														<label class="error" id="err_password"></label>
 													</div>
 												</div>
-												<div class="form-group">
-													<label class="col-md-3 control-label" for="profileConfirmNewPassword">Confirm New Password</label>
-													<div class="col-md-8">
-														<input type="text" class="form-control" id="profileConfirmNewPassword" >
-													</div>
+												<!-- New Password -->
+												<div id="new-password-form">
+													
 												</div>
 											</fieldset>
 											<div class="panel-footer">
+
 												<div class="row">
-													<div class="col-md-9 col-md-offset-3">
-														<button type="submit" class="btn btn-primary">Submit</button>
-														<button type="reset" class="btn btn-default">Reset</button>
+													<div class="col-sm-12 text-right">
+													<button type="button" id="update_profile" class="btn btn-primary mb-xs mt-xs mr-xs ">Update Profile</button>
+														
+													</div>
+													
 													</div>
 												</div>
 											</div>
@@ -218,8 +251,16 @@
 
     </section>
 		<?php include('../component/jslink.php');  ?>
-		<script src="../assets/vendor/jquery-maskedinput/jquery.maskedinput.js"></script>
-    
-    <?php include('../component/themejslink.php');  ?>
+		<!-- Specific Page Vendor -->
+		<script src="../assets/vendor/jquery-validation/jquery.validate.js"></script>
+		<script src="../assets/vendor/bootstrap-wizard/jquery.bootstrap.wizard.js"></script>
+		<script src="../assets/vendor/pnotify/pnotify.custom.js"></script>
+
+
+		<?php include('../component/themejslink.php');  ?>
+
+		<!-- Examples -->
+		<!-- <script src="../assets/javascripts/forms/examples.wizard.js"></script> -->
+		<script src="../assets/vendor/jquery-ui/js/jquery-ui.1.12.1.js"></script> 
   </body>
 </html>
