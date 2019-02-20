2c392a5a27996acac5cf2669e57c09d431bd3007
johncris09 <jcmanabo@gmail.com>
2019-02-20 23:15:42 +0800

    adding object to edit the selected resident

--- staff/edit-resident.php
+++ staff/edit-resident.php
@@ -1,3 +1,18 @@
+<?php session_start(); ?>
+<?php
+
+  include_once '../classes/Database.php';
+  include_once '../classes/BarangayStreet.php'; 
+  include_once '../classes/ResidenceHousehold.php';
+  include_once '../classes/initial.php'; 
+
+  $barangay_street = new BarangayStreet($db);
+  $residence_household = new ResidenceHousehold($db);
+
+  $person_id = $_SESSION['person_id'];
+  
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,6 +21,8 @@
   <?php include('../component/metadata.php'); ?>
   
   <!-- Specific Page Vendor CSS -->
+  <link rel="stylesheet" href="../assets/vendor/bootstrap-datepicker/css/datepicker3.css" />
+
   <link rel="stylesheet" href="../assets/vendor/pnotify/pnotify.custom.css" />
   <?php include('../component/csslink.php'); ?>
 </head>
@@ -47,7 +64,7 @@
                     <a href="#" class="fa fa-times"></a>
                   </div>
           
-                  <h2 class="panel-title">Add New Resident</h2>
+                  <h2 class="panel-title">Edit Resident</h2>
                 </header>
                 <div class="panel-body">
                   <div class="wizard-progress wizard-progress-lg">
@@ -68,67 +85,66 @@
                     <div class="tab-content">
                       
                       <!-- Person Information -->
-                      <div id="w4-profile" class="tab-pane active">
+                      <div id="w4-profile" class="tab-pane active ">
+                        
+                        <input type="hidden" value="<?php echo $person_id ?>" id="person_id">
+                        <input type="hidden" id="household_id">
+
                         <div class="form-group">
-                          <label class="col-sm-3 control-label" for="FirstName">First Name</label>
+                          <label class="col-sm-3 control-label" for="first_name">First Name</label>
                           <div class="col-sm-7">
-                            <input type="text" class="form-control" placeholder="First Name" name="first_name" id="FirstName" required="" autofocus>
+                            <input type="text" class="form-control" placeholder="First Name" name="first_name" id="first_name" required="" autofocus>
                           </div>
                         </div>
                         <div class="form-group">
-                          <label class="col-sm-3 control-label" for="MiddleName">Middle Name</label>
+                          <label class="col-sm-3 control-label" for="middle_name">Middle Name</label>
                           <div class="col-sm-7">
-                            <input type="text" class="form-control"  placeholder="Midde Name" name="middle_name" id="MiddleName" >
+                            <input type="text" class="form-control"  placeholder="Midde Name" name="middle_name" id="middle_name" >
                           </div>
                         </div>
                         <div class="form-group">
-                          <label class="col-sm-3 control-label" for="LastName">Last Name</label>
+                          <label class="col-sm-3 control-label" for="last_name">Last Name</label>
                           <div class="col-sm-7">
-                            <input type="text" class="form-control" placeholder="Last Name" name="last_name" id="LastName" required="">
+                            <input type="text" class="form-control" placeholder="Last Name" name="last_name" id="last_name" required="">
                           </div>
                         </div>
                         <div class="form-group">
-                          <label class="col-sm-3 control-label" for="Extension">Extension</label>
+                          <label class="col-sm-3 control-label" for="extension">Name Extension</label>
                           <div class="col-sm-3">
-                            <input type="text" class="form-control"  placeholder="Extension" name="extension" id="Extension">
+                            <input type="text" class="form-control"  placeholder="Extension" name="extension" id="extension">
                           </div>
                         </div>
                         <div class="form-group">
-                          <label class="col-sm-3 control-label" for="HouseNumber">House Number</label>
+                          <label class="col-sm-3 control-label" for="house_number">House Number</label>
                           <div class="col-sm-3">
-                            <input type="text" class="form-control" placeholder="House Number" name="housenumber" id="HouseNumber">
+                            <input type="text" class="form-control" placeholder="House Number" name="house_number" id="house_number" required="required">
                           </div>
                         </div>
                         <div class="form-group">
-                          <label class="col-sm-3 control-label" for="Birtplace">Birthplace</label>
+                          <label class="col-sm-3 control-label" for="birthplace">Birthplace</label>
                           <div class="col-sm-7">
-                            <input type="text" class="form-control"  placeholder="Birthplace" name="birthplace" id="Birtplace">
+                            <textarea name="birthplace" id="birthplace"  class="form-control" rows="3" placeholder="Birthplace" required="required"></textarea>
                           </div>
                         </div>
                         
                         <div class="form-group">
-                          <label class="col-sm-3 control-label" for="date">Birthdate</label>
-                          <div class="col-md-6">
+                          <label class="col-sm-3 control-label" for="birthdate">Birthdate</label>
+                          <div class="col-sm-7">
                             <div class="input-group">
                               <span class="input-group-addon">
                                 <i class="fa fa-calendar"></i>
                               </span>
-                              <input  id="date"  data-date-format="yyyy/mm/dd" name="birthdate" data-plugin-datepicker="" placeholder="YYYY/MM/DD" class="form-control" required>
+                              <input type="date" id="birthdate" class="form-control" required="">
+                              <!-- <input id="birthdate" data-plugin-masked-input="" data-input-mask="9999-99-99" placeholder="yyyy-mm-dd" class="form-control" required="required"> -->
                             </div>
-                            <label class="error" for="date"></label>
+                            <label class="error" for="birthdate"></label>
                           </div>
                         </div>
-                        <!-- <div class="form-group">
-                          <label class="col-sm-3 control-label" for="date">Birthdate</label>
-                          <div class="col-sm-3">
-                            <input id="date" data-plugin-masked-input="" data-input-mask="9999/99/99"  name="birthplace" placeholder="YYYY/MM/DD" class="form-control" required>
-                          </div>
-                        </div> -->
                         <div class="form-group">
-                          <label class="col-sm-3 control-label" for="w4-gender">Gender</label>
+                          <label class="col-sm-3 control-label" for="gender">Gender</label>
                           <div class="col-sm-9">
                             <div class="radio-custom radio-primary">
-                              <input id="male" name="gender" type="radio" value="Male" required="">
+                              <input id="male" name="gender" type="radio" value="Male" required="" checked="checked">
                               <label for="male">Male</label>
                             </div>
                             <div class="radio-custom radio-primary">
@@ -139,10 +155,9 @@
                           </div>
                         </div>
                         <div class="form-group">
-                          <label class="col-sm-3 control-label" for="status">Status</label>
+                          <label class="col-sm-3 control-label" for="status">Civil Status</label>
                           <div class="col-sm-3">
                             <select id="status" name="status" class="form-control" required="">
-                              <option value="">Choose Status</option>
                               <option value="Single">Single</option>
                               <option value="Married">Married</option>
                               <option value="Widowed">Widowed</option>
@@ -153,13 +168,13 @@
                         </div>
                         
                         <div class="form-group">
-                          <label class="col-sm-3 control-label" for="Citizenship">Citizenship</label>
+                          <label class="col-sm-3 control-label" for="citizenship">Citizenship</label>
                           <div class="col-sm-3">
-                            <input type="text" class="form-control" placeholder="Citizenship" name="citizenship" id="Citizenship" required="">
+                            <input type="text" class="form-control" placeholder="Citizenship" name="citizenship" id="citizenship" required="">
                           </div>
                         </div>
                           
-                        <div class="form-group">
+                        <!-- <div class="form-group">
 													<label class="col-md-3 control-label" for="PhoneNumber">Phone Number</label>
 													<div class="col-sm-4">
                             <div class="input-group">
@@ -169,14 +184,22 @@
                               <input id="PhoneNumber" name="phone_number" data-plugin-masked-input="" data-input-mask="(+63) 999-999-9999" placeholder="(+63) 123-123-1234" class="form-control">
                             </div>
                           </div>
-                        </div>
+                        </div> -->
                         <div class="form-group">
-													<label class="col-md-3 control-label" for="occupation">Occupation</label>
+													<label class="col-sm-3 control-label" for="occupation">Occupation</label>
 													<div class="col-sm-4">
-															<input id="occupation" name="occupation" placeholder="Occupation" class="form-control">
+                            <select class="form-control" id="occupation" required="required"> 
+                              <option value="Student">Student</option>
+                              <option value="Teacher">Teacher</option>
+                              <option value="Construction Worker">Construction Worker</option>
+                              <option value="Tricycle Driver">Tricycle Driver</option>
+                              <option value="Fruit Vendor">Fruit Vendor</option>
+                              <option value="Farmer">Farmer</option>
+                              <option value="Other">Other...</option>
+                            </select>
 													</div>
 												</div>
-                        <div class="form-group">
+                        <!-- <div class="form-group">
                           <label class="col-sm-3 control-label" for="ProfilePicture">Profile Picture</label>
                           <div class="row">
                             <div class="col-md-4 col-xs-8 col-lg-3">
@@ -190,28 +213,30 @@
                               </section>
                             </div>
                           </div>
-                        </div>
+                        </div> -->
                       </div>
                       
                       <!-- Household  -->
-                      <div id="w4-household" class="tab-pane ">
+                      <div id="w4-household" class="tab-pane  ">
+                        
                         <div class="form-group">
-                          <label class="col-md-3 control-label" for="Street">Street </label>
-                          <div class="col-md-7">
-                            <select id="Street" name="street" data-plugin-selectTwo class="form-control populate placeholder" data-plugin-options='{ "placeholder": "Select a State", "allowClear": true }' required>
-                              <optgroup label="Selected Barangay">
-                                <option value="">Select Street</option>
-                                <? for($i=1;$i<=5;$i++){?>
-                                <option value="Purok <? echo $i;?>">Purok <? echo $i; ?></option>
-                                <? } ?>
-                              </optgroup>
+                          <label class="col-sm-3 control-label" for="street">Street </label>
+                          <div class="col-sm-7">
+                            <select id="street" name="street" data-plugin-selectTwo class="form-control populate placeholder" data-plugin-options='{ "placeholder": "Select a State", "allowClear": true }' required>
+                          <?php
+                            $prep_state = $barangay_street->fetchSpecificStreetName($user_id);
+                            while($row = $prep_state->fetch(PDO::FETCH_ASSOC)) { ?>
+                              <option value="<?php echo $row['street_id'];?>"><?php echo $row['street_name']; ?></option>
+                          <?php
+                            }                          
+                          ?>                            
                             </select>
                           </div>
                         </div>
                         <div class="form-group">
-                          <label class="col-sm-3 control-label" for="HouseholdNumber">Household Number</label>
+                          <label class="col-sm-3 control-label" for="household_number">Household Number</label>
                           <div class="col-sm-7">
-                            <input type="text" class="form-control" placeholder="Household Number" name="household_number" id="HouseholdNumber" required="">
+                            <input type="text" class="form-control" placeholder="Household Number" name="household_number" id="household_number" required="">
                           </div>
                         </div>
                       </div> 
@@ -248,15 +273,20 @@
   </section>
   <?php include('../component/jslink.php');  ?>
   <!-- Specific Page Vendor -->
+  
   <script src="../assets/vendor/jquery-validation/jquery.validate.js"></script>
   <script src="../assets/vendor/bootstrap-wizard/jquery.bootstrap.wizard.js"></script>
   <script src="../assets/vendor/pnotify/pnotify.custom.js"></script>
   <script src="../assets/vendor/jquery-maskedinput/jquery.maskedinput.js"></script>
-  <script src="../assets/vendor/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
+
+
+
+ 
   <?php include('../component/themejslink.php');  ?>
 
   <!-- Examples -->
-  <script src="../assets/javascripts/forms/examples.wizard.js"></script>
+  <!-- <script src="../assets/javascripts/forms/examples.wizard.js"></script> -->
+  <script src="../assets/vendor/jquery-ui/js/jquery-ui.1.12.1.js"></script> 
 
 </body>
 </html>
