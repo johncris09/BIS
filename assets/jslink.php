caa24c6683903d1ebff4f27f5466f19aa09b233d
johncris09 <jcmanabo@gmail.com>
2019-02-20 23:09:12 +0800

    css link for login

---
+++ assets/jslink.php
@@ -0,0 +1,13 @@
+
+	<!-- Vendor -->
+	<script src=" assets/vendor/jquery/jquery.js"></script>
+		
+		<script src=" assets/vendor/jquery-browser-mobile/jquery.browser.mobile.js"></script>
+		<script src=" assets/vendor/bootstrap/js/bootstrap.js"></script>
+		<script src=" assets/vendor/nanoscroller/nanoscroller.js"></script>
+		<!-- <script src=" assets/vendor/bootstrap-datepicker/js/bootstrap-datepicker.js"></script> -->
+		<script src=" assets/vendor/magnific-popup/magnific-popup.js"></script>
+		<script src=" assets/vendor/jquery-placeholder/jquery.placeholder.js"></script>
+		<script src="assets/vendor/jquery-validation/jquery.validate.min.js"></script>
+		
+		
