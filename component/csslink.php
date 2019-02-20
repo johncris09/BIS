ff59244149606ba14db1a671b5df7edd9020cd17
johncris09 <jcmanabo@gmail.com>
2019-02-20 22:56:16 +0800

    css link for theme

--- component/csslink.php
+++ component/csslink.php
@@ -1,31 +1,28 @@
 
-		<!-- Mobile Metas -->
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+	<!-- Mobile Metas -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
-		<!-- Web Fonts  -->
-		<link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800|Shadows+Into+Light" rel="stylesheet" type="text/css">
+	<!-- Web Fonts  -->
+	<link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800|Shadows+Into+Light" rel="stylesheet" type="text/css">
 
-		<!-- Vendor CSS -->
-		<link rel="stylesheet" href="../assets/vendor/bootstrap/css/bootstrap.css" />
-		<link rel="stylesheet" href="../assets/vendor/font-awesome/css/font-awesome.css" />
-		<link rel="stylesheet" href="../assets/vendor/magnific-popup/magnific-popup.css" />
-		<link rel="stylesheet" href="../assets/vendor/bootstrap-datepicker/css/datepicker3.css" />
+	<!-- Vendor CSS -->
+	<link rel="stylesheet" href="../assets/vendor/bootstrap/css/bootstrap.css" />
+	<link rel="stylesheet" href="../assets/vendor/font-awesome/css/font-awesome.css" />
+	<link rel="stylesheet" href="../assets/vendor/magnific-popup/magnific-popup.css" />
+	<link rel="stylesheet" href="../assets/vendor/bootstrap-datepicker/css/datepicker3.css" />
 
 
 
-		<!-- Theme CSS -->
-		<link rel="stylesheet" href="../assets/stylesheets/theme.css" />
+	<!-- Theme CSS -->
+	<link rel="stylesheet" href="../assets/stylesheets/theme.css" />
 
-		<!-- Skin CSS -->
-		<link rel="stylesheet" href="../assets/stylesheets/skins/default.css" />
+	<!-- Skin CSS -->
+	<link rel="stylesheet" href="../assets/stylesheets/skins/default.css" />
 
-		<!-- Theme Custom CSS -->
-		<link rel="stylesheet" href="../assets/stylesheets/theme-custom.css">
+	<!-- Theme Custom CSS -->
+	<link rel="stylesheet" href="../assets/stylesheets/theme-custom.css">
 
-		<!-- Head Libs -->
-		<script src="../assets/vendor/modernizr/modernizr.js"></script>
+	<!-- Head Libs -->
+	<script src="../assets/vendor/modernizr/modernizr.js"></script>
 
-		
-		
-    
-    
+	
