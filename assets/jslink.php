60349f313a2103f9f13b16d3f19390b7e6929b4c
johncris09 <jcmanabo@gmail.com>
2019-02-27 19:58:07 +0800

    login js

--- assets/jslink.php
+++ assets/jslink.php
@@ -5,9 +5,8 @@
 		<script src=" assets/vendor/jquery-browser-mobile/jquery.browser.mobile.js"></script>
 		<script src=" assets/vendor/bootstrap/js/bootstrap.js"></script>
 		<script src=" assets/vendor/nanoscroller/nanoscroller.js"></script>
-		<!-- <script src=" assets/vendor/bootstrap-datepicker/js/bootstrap-datepicker.js"></script> -->
+		<script src=" assets/vendor/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
 		<script src=" assets/vendor/magnific-popup/magnific-popup.js"></script>
 		<script src=" assets/vendor/jquery-placeholder/jquery.placeholder.js"></script>
 		<script src="assets/vendor/jquery-validation/jquery.validate.min.js"></script>
-		
 		
