0862eaec83104a570ea26162698ff6cc019ccae7
johncris09 <jcmanabo@gmail.com>
2019-03-18 20:53:09 +0800

    add background image

--- index.php
+++ index.php
@@ -42,7 +42,17 @@
       h3.text-primary{
         font-family: 'Coiny', cursive;
       }
-      
+      body{
+        background-image: url('assets/images/background-1.jpg');
+        
+        /* Full height */
+        height: 100%; 
+
+        /* Center and scale the image nicely */
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+      }
     </style>
   </head>
   <body>
